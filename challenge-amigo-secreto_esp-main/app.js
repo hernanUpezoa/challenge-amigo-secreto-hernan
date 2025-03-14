e7712541bed42e798ef47c9b90586541a58af5b6
hernanUpezoa <pezoa.u.hernan@gmail.com>
2025-03-14 12:34:04 -0300

    Se modifica archivo .js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,48 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigo = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+    
+    console.log(nombreAmigo);
+
+    if(!nombreAmigo){
+        alert("Debes ingresar un nombre, ¿acaso no tienes amigos? :P");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    console.log(amigo);
+    inputAmigo.value = "";
+    inputAmigo.focus(); //deja el cursor listo para seguir escribiendo
+    renderizarAmigos();
+}
+
+function renderizarAmigos(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for(i = 0; i < amigo.length; i++){
+        let item = document.createElement("li");
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+
+}
+
+function sortearAmigo(){
+    if(amigo.length === 0){
+        alert("No hay amigos para sortear");
+        return;
+    }
+let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+let resultado = document.getElementById("resultado");
+resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+let limpiarLista = document.getElementById("listaAmigos");
+limpiarLista.innerHTML = "";
+
+}
+
+
+
